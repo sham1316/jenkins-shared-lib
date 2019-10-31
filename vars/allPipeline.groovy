81fb311a526b0559ed9536d16ed5f0d3076c0874
Alexey Pishchulin <alexey.pishchulin@gmail.com>
2019-11-01 00:53:34 +0300

    allPipeline

---
+++ vars/allPipeline.groovy
@@ -0,0 +1,37 @@
+def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent any
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+            stage('Get Dockerfile') {
+                steps {
+                    script {
+                        def tmpFile = libraryResource 'Dockerfile'
+                        writeFile file: 'Dockerfile', text: tmpFile
+                    }
+                }
+            }
+            stage('Build') {
+                steps {
+                    script {
+                        sh '''
+              docker version
+              docker build -t ealebed/hellonode:latest .
+              docker image ls
+            '''
+                    }
+                }
+            }
+        }
+    }
+}
