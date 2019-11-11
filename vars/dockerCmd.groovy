4c4882cf08553e144b4c6a8934ecc8ef18a3940c
Alexey Pishchulin <alexey.pishchulin@gmail.com>
2019-11-11 13:56:15 +0300

    add BuildConfig

--- vars/dockerCmd.groovy
+++ vars/dockerCmd.groovy
@@ -1,17 +1,8 @@
+def call(String cmd, String args, def optional = [:]) {
+    config = BuildConfig.resolve(optional)
 
-def call(def body = [:]) {
-    config = BuildConfig.resolve(body)
-
-    println(config)
-    assert args != null
-    assert cmd != null
-    assert withoutCache != null
-    if (cmd.equals("build")) {
-        if (withoutCache.equals("yes")) {
-            args = "--no-cache " + args
-            sh(script: "docker ${cmd} ${args}")
-        }
-    } else {
-        sh(script: "docker ${cmd} ${args}")
+    if (config.noCache == "yes") {
+        args = "--no-cache " + args;
     }
+    sh(script: "docker ${cmd} ${args}")
 }
